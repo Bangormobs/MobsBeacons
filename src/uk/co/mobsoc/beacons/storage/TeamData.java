0fbfb2ae3ac9ae94cad52cbd8e95fb5fafbf8ff9
trigg <triggerhapp@gmail.com>
2014-06-05 21:40:43 +0100

    - Added getters/setters to TeamData

--- src/uk/co/mobsoc/beacons/storage/TeamData.java
+++ src/uk/co/mobsoc/beacons/storage/TeamData.java
@@ -1,9 +1,19 @@
 package uk.co.mobsoc.beacons.storage;
 
-import java.util.ArrayList;
-
 public class TeamData {
 	private int teamId = -1;
 	private String teamName = "";
+	public int getTeamId() {
+		return teamId;
+	}
+	public void setTeamId(int teamId) {
+		this.teamId = teamId;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
 	
 }
