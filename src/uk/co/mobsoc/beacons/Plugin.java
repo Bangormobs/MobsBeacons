51847143782bf49b803de332ab0b586f1e0c47c1
trigg <triggerhapp@gmail.com>
2014-06-06 22:59:24 +0100

    - Added config reading of MySQL details

--- src/uk/co/mobsoc/beacons/Plugin.java
+++ src/uk/co/mobsoc/beacons/Plugin.java
@@ -5,12 +5,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import uk.co.mobsoc.beacons.listener.BeaconListener;
 import uk.co.mobsoc.beacons.listener.EventListen;
+import uk.co.mobsoc.beacons.storage.MySQL;
 import uk.co.mobsoc.beacons.transientdata.Event;
 
 public class Plugin extends JavaPlugin {
 	public static World world;
+	private String userName, passWord, dataBase, IP;
 	@Override
 	public void onEnable(){
+		readConfig();
+		MySQL.prepareMySQL(IP, userName, passWord , dataBase);
 		new EventListen(this);
 		new BeaconListener(this);
 	}
@@ -28,5 +32,14 @@ public class Plugin extends JavaPlugin {
 		if(Plugin.event != null){
 			Plugin.event = event;
 		}
+	}
+	
+	private void readConfig() {
+		saveDefaultConfig();
+		userName = getConfig().getString("sql-username");
+		passWord = getConfig().getString("sql-password");
+		dataBase = getConfig().getString("sql-database");
+		IP = getConfig().getString("sql-ip");
+
 	}
 }
