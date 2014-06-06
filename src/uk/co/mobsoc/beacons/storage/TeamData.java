6dd8e94a93ad99d5514285ea0da2e7d4d7676465
trigg <triggerhapp@gmail.com>
2014-06-07 00:10:01 +0100

    - Added TeamData constructor from MySQL ResultSet

--- src/uk/co/mobsoc/beacons/storage/TeamData.java
+++ src/uk/co/mobsoc/beacons/storage/TeamData.java
@@ -1,8 +1,22 @@
 package uk.co.mobsoc.beacons.storage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TeamData {
 	private int teamId = -1;
 	private String teamName = "";
+	public TeamData(ResultSet rs) {
+		try {
+			teamId = rs.getInt(1);
+			teamName = rs.getString(2);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public TeamData(){
+	}
 	public int getTeamId() {
 		return teamId;
 	}
