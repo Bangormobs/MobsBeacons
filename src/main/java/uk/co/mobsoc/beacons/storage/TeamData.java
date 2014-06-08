8adc904acb4a473d8d51792b8da1432fead44dd6
trigg <triggerhapp@gmail.com>
2014-06-08 22:36:09 +0100

    - Added score to TeamData

--- src/main/java/uk/co/mobsoc/beacons/storage/TeamData.java
+++ src/main/java/uk/co/mobsoc/beacons/storage/TeamData.java
@@ -4,13 +4,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TeamData {
-	private int teamId = -1;
+	private int teamId = -1, score=0;
 	private String teamName = "";
 	public TeamData(ResultSet rs) {
 		try {
 			teamId = rs.getInt(1);
 			teamName = rs.getString(2);
-
+			score = rs.getInt(3);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -28,6 +28,12 @@ public class TeamData {
 	}
 	public void setTeamName(String teamName) {
 		this.teamName = teamName;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
 	}
 	
 }
